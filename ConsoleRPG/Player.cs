f715182a54f917be71e96ffc161f5af4268117ce
MaciekLabedzki <lamacho123@gmail.com>
2020-03-18 18:16:58 +0100

    Added basic classes

---
+++ ConsoleRPG/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleRPG.Map;
+
+namespace ConsoleRPG.PlayerCharacter
+{
+    public class Player
+    {
+        public Position position;
+        public char Representative;
+    }
+}
---
+++ ConsoleRPG/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleRPG.Map
+{
+    class Tile
+    {
+        public Position Position;
+        public char Terrain;
+
+        public bool Walkable;
+
+        public Tile()
+        {
+
+        }
+    }
+}
